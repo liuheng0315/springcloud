34be6fbe6ce88dfa41dfc942a2085ce9fa08dad3
liuheng <liuheng0315@gmail.com>
2018-11-22 17:42:51 +0800

    增加eureka集群配置

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -19,7 +19,8 @@ public class DeptController
 {
 	@Autowired
 	private DeptService service;
-
+	@Autowired
+	private DiscoveryClient client;
 
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept)
@@ -39,5 +40,18 @@ public class DeptController
 		return service.list();
 	}
 
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
 
 }
