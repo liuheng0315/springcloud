39703353ff9dabd9626ed0050ab1f2edbd2b78fa
liuheng <liuheng0315@gmail.com>
2018-11-23 14:21:52 +0800

    实现rebbon负载均衡机制

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
     }
 }
